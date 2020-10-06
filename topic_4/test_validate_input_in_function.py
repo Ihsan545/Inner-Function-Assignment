8540d9aeef0678bbc6cf188c9f815ae1f69b6af2
anwar <Ihsan545>
2020-10-05 20:34:55 -0500

    test failed

--- topic_4/test_validate_input_in_function.py
+++ topic_4/test_validate_input_in_function.py
@@ -6,6 +6,9 @@ class MyTestCase(unittest.TestCase):
     def test_score_valid(self):
         self.assertTrue(validate_input_in_function.score_input("python", 7))
 
+    def test_above_range_input(self):
+        self.assertTrue(validate_input_in_function.score_input("Invalid value, above the range"), 45)
+
 
 
 
